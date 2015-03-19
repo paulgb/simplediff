264cd5fd66a4dbaab9f40029c772105a67daf595
Philippe Fremy <phil@freehackers.org>
2015-03-19 22:39:32 +0100

    Add tests with lists of words

--- java/src/SimpleDiffTest.java
+++ java/src/SimpleDiffTest.java
@@ -1,11 +1,14 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
@@ -13,9 +16,9 @@ import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
 
 public class SimpleDiffTest {
 
-	static String diffDataStringOld = "The quick brown fox.";
-	static String diffDataStringNew = "The kuick brown fix.";
-    static String[][] diffDataStringExpected = {
+	static String diffDataCharOld = "The quick brown fox.";
+	static String diffDataCharNew = "The kuick brown fix.";
+    static String[][] diffDataCharExpected = {
     		{ "=", "The " },
     		{ "-", "q" },
             { "+", "k" },
@@ -26,24 +29,69 @@ public class SimpleDiffTest {
 	};
 	
 	@Test
-	public void testListCharToString() {
-		Character[] listChar = { 't', 'o', 't', 'o' };
-		assertEquals( SimpleDiff.listCharToString( Arrays.asList(listChar) ), "toto" );
-	}
-	
-	@Test
 	public void testDiffString() {
 		List< Map.Entry<SimpleDiff.DiffOp, String > > ret;
 		List< Map.Entry<SimpleDiff.DiffOp, String > > expected;
-		ret = SimpleDiff.diffString(diffDataStringOld, diffDataStringNew);
+		ret = SimpleDiff.diffString(diffDataCharOld, diffDataCharNew);
 		
-		expected = Arrays.asList( diffDataStringExpected ).stream()
+		expected = Arrays.asList( diffDataCharExpected ).stream()
 			.map(ar -> new AbstractMap.SimpleEntry<SimpleDiff.DiffOp, String>(
 					SimpleDiff.DiffOp.fromString(ar[0]), ar[1]) )
 			.collect(Collectors.toList());
 		assertEquals(expected, ret);;
 		
 	}
+    
+    static String[] diffDataWordOld1 = { "The", "quick", "brown", "fox" };
+    static String[] diffDataWordNew1 = { "The", "slow", "green", "turtle" };
+    static String[][] diffDataWordExpected1 = {
+      	{"=", "The"},
+        {"-", "quick", "brown", "fox"},
+        {"+", "slow", "green", "turtle"},
+    };
+    
+    static String[] diffDataWordOld2 = { "jumps", "over", "the", "lazy", "dog" };
+    static String[] diffDataWordNew2 = { "walks", "around", "the", "orange", "cat" };
+    static String[][] diffDataWordExpected2 = {
+      {"-", "jumps", "over" },
+      {"+", "walks", "around" },
+      {"=", "the" },
+      {"-", "lazy", "dog" },
+      {"+", "orange", "cat" },
+    };
+    
+	@Test
+	public void testDiffWord1() {
+		subDiffWord(diffDataWordOld1, diffDataWordNew1, diffDataWordExpected1 );
+	}
+	
+	@Test
+	public void testDiffWord2() {
+		subDiffWord(diffDataWordOld2, diffDataWordNew2, diffDataWordExpected2 );
+	}
+	
+	public void subDiffWord( String[] oldWord, String[] newWord, String[][] expectedWord ) {
+		List< Map.Entry<SimpleDiff.DiffOp, List<String> > > ret;
+		List< Map.Entry<SimpleDiff.DiffOp, List<String> > > expected;
+		ret = SimpleDiff.diff(Arrays.asList(oldWord), Arrays.asList(newWord));
+		
+		expected = Arrays.asList( expectedWord ).stream()
+			.map(ar -> new AbstractMap.SimpleEntry<SimpleDiff.DiffOp, List<String> >(
+					SimpleDiff.DiffOp.fromString(ar[0]), 
+						Arrays.asList(ar).subList(1, ar.length) ) )
+			.collect(Collectors.toList());
+		assertEquals(expected, ret);
+	}
+	
+
+
+	@Test
+	public void testListCharToString() {
+		Character[] listChar = { 't', 'o', 't', 'o' };
+		assertEquals( SimpleDiff.listCharToString( Arrays.asList(listChar) ), "toto" );
+	}
+	
+
 
 }
 
