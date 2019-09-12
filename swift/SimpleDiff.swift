7ff88ba5d413424339e4f995b85c0cc9fb7f6e6c
Michael Henry <me@iamkel.net>
2019-09-13 02:38:09 +0900

    include assertion

--- swift/SimpleDiff.swift
+++ swift/SimpleDiff.swift
@@ -1,15 +1,13 @@
-import Foundation
-
 class SimpleDiff {
-
+  
   enum DiffType<T> {
     case insert([T])
     case delete([T])
     case equal([T])
   }
-
+  
   func diff<T>(_ old: [T], with new: [T]) -> [DiffType<T>] where T: Hashable {
-
+    
     // Create a map from old values to their indices
     var oldIndexMap = [T: [Int]]()
     for (i, val) in old.enumerated() {
@@ -17,12 +15,12 @@ class SimpleDiff {
       indexes.append(i)
       oldIndexMap[val] = indexes
     }
-
+    
     var subStartOld = 0
     var subStartNew = 0
     var subLength = 0
-
-    var overlap = [Int: Int]()
+    
+    let overlap = [Int: Int]()
     for (iNew, val) in new.enumerated() {
       for iOld in oldIndexMap[val] ?? [] {
         let _subLength = (overlap[iOld - 1] ?? 0) + 1
@@ -33,7 +31,7 @@ class SimpleDiff {
         }
       }
     }
-
+    
     if subLength == 0 {
       return (old.count > 0 ? [.delete(old)] : []) + (new.count > 0 ? [.insert(new)] : [])
     } else {
@@ -44,4 +42,31 @@ class SimpleDiff {
       return _diffs
     }
   }
+ 
 }
+
+
+extension SimpleDiff.DiffType:CustomStringConvertible where T == String {
+  
+  var description: String {
+    switch self {
+    case .insert(let values):
+      return "+\(values.joined(separator: ""))"
+    case .delete(let values):
+      return "-\(values.joined(separator: ""))"
+    case .equal(let values):
+      return "=\(values.joined(separator: ""))"
+    }
+  }
+}
+
+let simpleDiff = SimpleDiff()
+let old = "hello world!"
+let new = "hi world?"
+
+let result = simpleDiff.diff(old.map { "\($0)" }, with: new.map { "\($0)" })
+  .reduce("", { (result, acc) -> String in
+  return result + acc.description
+})
+
+assert(result == "=h-ello+i= =w=o=r=l=d-!+?")
